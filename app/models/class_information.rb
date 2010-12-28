bdf135d730d568c97c5e3195ccd4b59f1a56b80d
sapthagiri <sapthagiri269@gmail.com>
2010-12-28 23:35:55 +0530

    Minor

--- app/models/class_information.rb
+++ app/models/class_information.rb
@@ -1,7 +1,6 @@
 class ClassInformation < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :class_number
-  validates_numericality_of :class_number, :only_integer => true, :message => "sholud be a number."
-  validates_inclusion_of :class_number, :in => 10000..99999, :message => "sholud be 5 digit number."
+  validates_numericality_of :class_number, :only_integer => true, :greater_than_or_equal_to => 10000, :less_than_or_equal_to => 99999, :message => "should be 5 digit number."  
   validates_presence_of :credits, :day, :start_time, :stop_time
 end
