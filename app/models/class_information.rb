97c634d833b8eb36acbc420fa4ac47244ef69e5c
sapthagiri <sapthagiri269@gmail.com>
2011-02-20 22:26:11 +0530

    Removed ClassInformations

--- app/models/class_information.rb
+++
@@ -1,6 +0,0 @@
-class ClassInformation < ActiveRecord::Base
-  belongs_to :user
-  validates_presence_of :class_number
-  validates_numericality_of :class_number, :only_integer => true, :greater_than_or_equal_to => 10000, :less_than_or_equal_to => 99999, :message => "should be 5 digit number."  
-  validates_presence_of :credits, :day, :start_time, :stop_time
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :students, :collection => {:class_information => :get, :csv => :get}
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -31,7 +33,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "users", :action => "class_form"
+  map.root :controller => "students", :action => "new"
 #  map.resources "users"
 
   # See how all your routes lay out with "rake routes"
